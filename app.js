e6e79fe3f858fcd34e6045dec9f942fdcb70e046
Sheersh01 <saxenasheersh1@gmail.com>
2025-10-31 17:26:02 +0530

    deploy

--- app.js
+++ app.js
@@ -227,6 +227,7 @@ io.on("connection", (uniqueSocket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000; // ✅ Render expects process.env.PORT
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
